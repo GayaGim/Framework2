274f0b6219f1d946a5d460b157803f0b65c6c08f
GayathriJayasinghe <g.g.jayasinghe@gmail.com>
2025-09-25 03:50:01 +0530

    Updated Login page - Constructor is added

--- Framework2/src/test/java/com/automation/pages/LoginPage.java
+++ Framework2/src/test/java/com/automation/pages/LoginPage.java
@@ -8,76 +8,75 @@ import com.automation.utilities.BrowserFactory;
 
 public class LoginPage {
 	public WebDriver driver;
-  
-	//constructor
-//  public LoginPage(WebDriver ldriver) {
-//	  this.driver = ldriver;
-//  }
-  
-  @FindBy(xpath="//label[text()='Username:']/following-sibling::input[@id='loginusername']")
-  WebElement loginUserName;
-  
-  @FindBy(xpath="//label[text()='Username:']/following-sibling::input[@id='sign-username']")
-  WebElement signUpUserName;
-  
-  @FindBy(xpath="//label[text()='Password:']/following-sibling::input[@id='loginpassword']")
-  WebElement loginPassword;
-  
-  @FindBy(xpath="//label[text()='Password:']/following-sibling::input[@id='sign-password']")
-  WebElement signUpPassword;
-  
-  @FindBy(xpath="//a[text()='Sign up']")
-  WebElement signUpLink;
-  
-  @FindBy(xpath="//button[text()='Sign up']")
-  WebElement signUpButton;
-  
-  @FindBy(xpath="//a[text()='Log in']")
-  WebElement loginlink;
-  
-  @FindBy(xpath="//button[text()='Log in']")
-  WebElement loginButton;
-  
-  @FindBy(xpath="//a[@id='nameofuser']")
-  WebElement nameOfUser;
-  
-  public void register(String uname, String upassword) {
-	  signUpLink.click();
-	  BrowserFactory.sleep(2);
-	  signUpUserName.sendKeys(uname);
-	  BrowserFactory.sleep(2);
-	  signUpPassword.sendKeys(upassword);
-	  BrowserFactory.sleep(2);
-	  signUpButton.click();
-	  BrowserFactory.sleep(2);
-	  	  
-  }
-  
-  public void login(String uname, String upassword) {
-	  loginlink.click();
-	  BrowserFactory.sleep(2);
-	  loginUserName.sendKeys(uname);
-	  BrowserFactory.sleep(2);
-	  loginPassword.sendKeys(upassword);
-	  BrowserFactory.sleep(2);
-	  loginButton.click();
-	  BrowserFactory.sleep(2);
-	  	  
-  }
-  public String nameOfUser() {
-	  BrowserFactory.sleep(2);
-	  String name = nameOfUser.getText();
-	  System.out.println(name);
-	  return name;
-  }
-  
-  public String successfulPopup(){
-	  BrowserFactory.sleep(2);
-	  String popupMessage = driver.switchTo().alert().getText();
-	  System.out.println(popupMessage);
-	  return popupMessage;
-  }
-  
-  
-  
+
+	// this is new commit - constructor
+	public LoginPage(WebDriver ldriver) {
+		this.driver = ldriver;
+	}
+
+	@FindBy(xpath = "//label[text()='Username:']/following-sibling::input[@id='loginusername']")
+	WebElement loginUserName;
+
+	@FindBy(xpath = "//label[text()='Username:']/following-sibling::input[@id='sign-username']")
+	WebElement signUpUserName;
+
+	@FindBy(xpath = "//label[text()='Password:']/following-sibling::input[@id='loginpassword']")
+	WebElement loginPassword;
+
+	@FindBy(xpath = "//label[text()='Password:']/following-sibling::input[@id='sign-password']")
+	WebElement signUpPassword;
+
+	@FindBy(xpath = "//a[text()='Sign up']")
+	WebElement signUpLink;
+
+	@FindBy(xpath = "//button[text()='Sign up']")
+	WebElement signUpButton;
+
+	@FindBy(xpath = "//a[text()='Log in']")
+	WebElement loginlink;
+
+	@FindBy(xpath = "//button[text()='Log in']")
+	WebElement loginButton;
+
+	@FindBy(xpath = "//a[@id='nameofuser']")
+	WebElement nameOfUser;
+
+	public void register(String uname, String upassword) {
+		signUpLink.click();
+		BrowserFactory.sleep(2);
+		signUpUserName.sendKeys(uname);
+		BrowserFactory.sleep(2);
+		signUpPassword.sendKeys(upassword);
+		BrowserFactory.sleep(2);
+		signUpButton.click();
+		BrowserFactory.sleep(2);
+
+	}
+
+	public void login(String uname, String upassword) {
+		loginlink.click();
+		BrowserFactory.sleep(2);
+		loginUserName.sendKeys(uname);
+		BrowserFactory.sleep(2);
+		loginPassword.sendKeys(upassword);
+		BrowserFactory.sleep(2);
+		loginButton.click();
+		BrowserFactory.sleep(2);
+
+	}
+
+	public String nameOfUser() {
+		BrowserFactory.sleep(2);
+		String name = nameOfUser.getText();
+		System.out.println(name);
+		return name;
+	}
+
+	public String successfulPopup() {
+		BrowserFactory.sleep(2);
+		String popupMessage = driver.switchTo().alert().getText();
+		System.out.println(popupMessage);
+		return popupMessage;
+	}
+
 }
